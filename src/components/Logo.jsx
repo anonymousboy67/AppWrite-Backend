e2fe847c8ef69fb39a92734912bd678e31ab4be0
anonymousboy67 <aashishad67@gmail.com>
2024-01-31 23:21:03 +0545

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Logo({width ="5%"}) {
+  return (
+    <img src='m.logo.jpg' style={{width}} alt='Logo placeholder' />
+  )
+}
+
+export default Logo
